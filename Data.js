eea3b514485e28852cbe9d04d3fa8625388da343
silbnarvay <silbnarvay@gmail.com>
2023-12-27 21:11:31 -0300

    carrito-app

---
+++ Data.js
@@ -0,0 +1,117 @@
+const data = {
+    status: "success",
+    items: [
+      {
+        id: 1,
+        title: "Deportivas color negro de nylon, suela alta",
+        price: 400,
+        image: require('./images/img01.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-Black-Nylon/Images/Nike-LD-Waffle-Sacai-Black-Nylon/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+        cantidad: 1,
+      },
+      {
+        id: 2,
+        title: "Casual combinadas verde  y coral",
+        price: 320,
+        image: require('./images/img02.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-Pine-Green/Images/Nike-Dunk-Low-Off-White-Pine-Green/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 3,
+        title: "zapatillas total black de cuero ecológico",
+        price: 410,
+        image: require('./images/img03.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Images/Nike-Air-Force-1-Low-Supreme-Box-Logo-Black/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 4,
+        title: "deportivas combinadas de nylon y cuero blancas",
+        price: 380,
+        image: require('./images/img04.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-LD-Waffle-Sacai-White-Nylon/Images/Nike-LD-Waffle-Sacai-White-Nylon/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 5,
+        title: "deportivas cuero azul claro y blanco",
+        price: 440,
+        image: require('./images/img05.jpg'),
+        category: "calzado",
+        img1: " https://stockx-360.imgix.net/Nike-Dunk-Low-SP-Kentucky/Images/Nike-Dunk-Low-SP-Kentucky/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 6,
+        title: "casual combinadas en coral y gris",
+        price: 311,
+        image: require('./images/img06.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Dunk-Low-Off-White-University-Red/Images/Nike-Dunk-Low-Off-White-University-Red/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 7,
+        title: "super deportivas multicolor, suela aerodinámica",
+        price: 360,
+        image: require('./images/img07.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Air-Max-2-Light-Atmos/Images/Nike-Air-Max-2-Light-Atmos/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 8,
+        title: "zapatillas azul claro de cuero ecológico",
+        price: 365,
+        image: require('./images/img08.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Air-Force-1-Low-Clot-Blue-Silk/Images/Nike-Air-Force-1-Low-Clot-Blue-Silk/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 9,
+        title: "zaptillas con suela vibram gris, lima, negro y blanco",
+        price: 470,
+        image: require('./images/img09.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Air-Max-90-OG-White-Particle-Grey-Volt/Images/Nike-Air-Max-90-OG-White-Particle-Grey-Volt/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1606323250&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 10,
+        title: "deportivas alta competencia refozadas, amarilla y negra",
+        price: 501,
+        image: require('./images/img10.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Dunk-High-Black-Varsity-Maize/Images/Nike-Dunk-High-Black-Varsity-Maize/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1609445259&w=1000",
+        cantidad: 1
+      },
+      {
+        id: 11,
+        title: "deportivas alta competencia, suela refozada",
+        price: 490,
+        image: require('./images/img011.jpg'),
+        category: "calzado",
+        img1: "https://stockx-360.imgix.net/Nike-Air-Rubber-Dunk-Off-White-UNC/Images/Nike-Air-Rubber-Dunk-Off-White-UNC/Lv2/img",
+        img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+        cantidad: 1
+      },
+    ],
+  };
+  
+  export default data;
---
+++ componentes/Productos/ProductoItem.js
@@ -0,0 +1,36 @@
+import React, {useContext} from 'react';
+import { Link } from "react-router-dom";
+import { DataContext } from "../../context/Dataprovider";
+
+
+export const ProductoItem = ({id, title, price, image, category }) => {
+
+    const value = useContext(DataContext);
+    const addCarrito = value.addCarrito;
+
+
+    return(
+    
+      <div key={id} className="producto">
+      <Link to={`/producto/${id}`}>
+      <div className="producto__img">
+        <img src={image} alt={title} />
+      </div>
+      </Link>
+      <div className="producto__footer">
+        <h1>{title}</h1>
+        <p>{category}</p>
+        <p className="price">${price} </p>
+      </div>
+      <div className="bottom">
+        <button onClick={() => addCarrito(id)} className="btn">Agregar al carrito</button>
+        <div>
+        <Link to={`/producto/${id}`} className="btn">Vista</Link>
+        </div>
+      </div>
+    </div>
+    
+    );
+};
+
+
